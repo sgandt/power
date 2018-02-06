26faf696b8512830e43956417e4ade82d4fb5fad
sgandt <35653041+sgandt@users.noreply.github.com>
2018-02-06 16:42:09 +0530

    Create gk.c

---
+++ gk.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int b,c,i;
+    clrscr();
+    printf("enter the value for b");
+    scanf("%d",&n);
+    for(i=0;b!=0;i++)
+    {
+        n/=10;
+        c=c+1;
+    }
+    printf("%d is c",c);
+    getch();
+}
